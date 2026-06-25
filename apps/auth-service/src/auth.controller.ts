c060182dff63c7b639d06152933245ad6ed8a217
Ishara Dhanushan <isharadh2002@gmail.com>
2026-06-25 21:50:38 +0530

    fix: fix unused var error

--- apps/auth-service/src/auth.controller.ts
+++ apps/auth-service/src/auth.controller.ts
@@ -47,6 +47,7 @@ export class AuthController {
       });
     }
 
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const { password, role, ...safe } = user;
     return {
       ...safe,

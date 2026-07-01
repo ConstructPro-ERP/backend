d5554e913f20cabc2ec2a280aea8fd3e87cc7a23
IsuruKRanasundara <ranasundaraisuru4@gmail.com>
2026-07-01 20:35:55 +0530

    feat(backend):configurations in package.json

--- apps/api-gateway/src/app.module.ts
+++ apps/api-gateway/src/app.module.ts
@@ -10,7 +10,6 @@ import { PaymentsGatewayController } from './controllers/payments-gateway.contro
 import { QuotationsGatewayController } from './controllers/quotations-gateway.controller';
 import { RagGatewayController } from './controllers/rag-gateway.controller';
 import { UsersGatewayController } from './controllers/users-gateway.controller';
-import { RoutesController } from './controllers/routes.controller';
 import { LeadGatewayController } from './controllers/lead-gateway.controller';
 import { ClientGatewayController } from './controllers/client-gateway.controller';
 
@@ -31,7 +30,6 @@ import { ClientGatewayController } from './controllers/client-gateway.controller
     PaymentsGatewayController,
     RagGatewayController,
     UsersGatewayController,
-    RoutesController,
     LeadGatewayController,
     ClientGatewayController,
   ],
